7c4ec36dfea68df68bb5f8a6340575fdb23eb72a
bagreefine <arthursou2007@gmail.com>
2026-08-28 14:25:45 -0300

    Atualiza codigo do Exercicios1.java

---
+++ ExerciciosJava/Exercicios1.java
@@ -0,0 +1,11 @@
+package ExerciciosJava;
+
+public class Exercicios1 {
+    
+    public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nome: Arthur Sousa");
+        System.out.println("Curso: Desenvolvimento de Software Multiplataforma");
+    }
+    }
+}
